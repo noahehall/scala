3baeba021904e0e910f4521dc798b1d2bfaab2fa
noahehall <noahehall@users.noreply.github.com>
2022-06-08 01:27:15 -0700

    simple fns

--- scala-3-project-template/src/main/scala/Main.scala
+++ scala-3-project-template/src/main/scala/Main.scala
@@ -4,11 +4,12 @@
   val poop = "flush"
   println(poop)
   println(sum(1, 1))
-  println(isTruthy(false))
-  println(isTruthy(true))
+  println(isTruthy(me = true)) // with named param
+  println(isTruthy()) // uses the default value defined in the fn signature
 
 def msg = "I was compiled by Scala 3. :)"
 
 def sum(n1: Int, n2: Int): Int = n1 + n2
 
-def isTruthy(me: Boolean): String = if me then "you are truthy" else "you are falsy"
+// has default value of false
+def isTruthy(me: Boolean = false): String = if me then "you are truthy" else "you are falsy"
