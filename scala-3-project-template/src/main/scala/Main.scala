096df4fc733af85ee8aba06c8de520bbd43828f5
noahehall <noahehall@users.noreply.github.com>
2022-06-08 01:16:52 -0700

    scala

--- scala-3-project-template/src/main/scala/Main.scala
+++ scala-3-project-template/src/main/scala/Main.scala
@@ -1,5 +1,14 @@
 @main def hello: Unit = 
   println("Hello world!")
   println(msg)
+  val poop = "flush"
+  println(poop)
+  println(sum(1, 1))
+  println(isTruthy(false))
+  println(isTruthy(true))
 
 def msg = "I was compiled by Scala 3. :)"
+
+def sum(n1: Int, n2: Int): Int = n1 + n2
+
+def isTruthy(me: Boolean): String = if me then "you are truthy" else "you are falsy"
