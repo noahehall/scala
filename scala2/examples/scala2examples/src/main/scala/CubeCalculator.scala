a35177b46a933c377af5b32fbe256d452d38c94c
Noah Hall <nohall@caesars.com>
2022-07-09 12:54:56 -0700

    scala2

--- scala2/examples/scala2examples/src/main/scala/CubeCalculator.scala
+++ scala2/examples/scala2examples/src/main/scala/CubeCalculator.scala
@@ -1,5 +1,6 @@
-object CubeCalculator extends App {
-  def cube(x: Int) = {
-    x * x * x
+// object Poop extends App {...}
+object Scala2Examples {
+  def main(args: Array[String]): Unit = {
+    println("scala fkn 2")
   }
 }
--- scala2/examples/scala2examples/src/test/scala/CubeCalculatorTest.scala
+++ scala2/examples/scala2examples/src/test/scala/CubeCalculatorTest.scala
@@ -1,5 +1,5 @@
-class CubeCalculatorTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("CubeCalculator.cube") {
-    assert(CubeCalculator.cube(3) === 27)
-  }
-}
+//class CubeCalculatorTest extends org.scalatest.funsuite.AnyFunSuite {
+//  test("CubeCalculator.cube") {
+//    assert(CubeCalculator.cube(3) === 27)
+//  }
+//}
